664344685cf9ac252d274b4952617a0bb45e90a7
projectormato <k.keita811.anuy@gmail.com>
2020-08-13 09:01:42 +0900

    JOOQ で自動生成するようにした

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,8 +1,11 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+import nu.studer.gradle.jooq.JooqEdition
 
 plugins {
 	id("org.springframework.boot") version "2.3.2.RELEASE"
 	id("io.spring.dependency-management") version "1.0.9.RELEASE"
+	id("nu.studer.jooq") version "5.0"
+	id("java")
 	kotlin("jvm") version "1.3.72"
 	kotlin("plugin.spring") version "1.3.72"
 }
@@ -21,6 +24,7 @@ dependencies {
 	implementation ("org.springframework.boot:spring-boot-starter-web")
 	implementation("org.jetbrains.kotlin:kotlin-reflect")
 	implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
+	jooqGenerator("org.postgresql:postgresql")
 	runtimeOnly("org.postgresql:postgresql")
 	annotationProcessor("org.projectlombok:lombok")
 	testImplementation("org.springframework.boot:spring-boot-starter-test") {
@@ -36,5 +40,44 @@ tasks.withType<KotlinCompile> {
 	kotlinOptions {
 		freeCompilerArgs = listOf("-Xjsr305=strict")
 		jvmTarget = "11"
+	}
+	// JOOQの自動生成をdependsOnする
+}
+
+jooq {
+	version.set("3.13.4")
+	edition.set(JooqEdition.OSS)
+
+
+	configurations {
+		create("jooq-gen") {
+			jooqConfiguration.apply {
+				jdbc.apply {
+					driver = "org.postgresql.Driver"
+					url = "jdbc:postgresql://localhost:5432/stock_db"
+					user = "projectormato"
+					password = "password"
+				}
+				generator.apply {
+					name = "org.jooq.codegen.DefaultGenerator"
+					database.apply {
+						name = "org.jooq.meta.postgres.PostgresDatabase"
+						includes = ".*"
+						excludes = ""
+					}
+					generate.apply {
+						isDeprecated = false
+						isRecords = false
+						isImmutablePojos = false
+						isFluentSetters = false
+					}
+					target.apply {
+						packageName = "com.projectormato.stockmanagement.gen"
+						directory = "$buildDir/jooq-gen"
+					}
+					strategy.name = "org.jooq.codegen.DefaultGeneratorStrategy"
+				}
+			}
+		}
 	}
 }
